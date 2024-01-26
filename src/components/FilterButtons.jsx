033f14f43df9af2c146d53c126f169740e259344
GNooNJo <jjjchoon@nate.com>
2024-01-26 20:24:08 +0900

    Refresh: ex4

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -1,27 +1,17 @@
 import React from "react";
 
 // TODO: FilterButtons 컴포넌트를 작성하세요. 필터링 옵션을 선택하는 버튼들을 포함해야 합니다.
-function FilterButtons(
-  /* 필요한 props를 여기에 전달하세요 */ { initialStudents, setFilteredStudents }
-) {
-  const filterByAge = (minAge) => {
-    const studentsFilteredByAge = initialStudents.filter((student) => student.age >= minAge);
-    setFilteredStudents(studentsFilteredByAge);
-  };
-  const filterByGrade = (grade) => {
-    const studentsFilteredByGrade = initialStudents.filter((student) => student.grade === grade);
-    setFilteredStudents(studentsFilteredByGrade);
-  };
-  const resetFilter = () => {
-    setFilteredStudents(initialStudents);
-  };
+function FilterButtons(/* 필요한 props를 여기에 전달하세요 */ {}) {
+  const filterByAge = (minAge) => {};
+  const filterByGrade = (grade) => {};
+  const resetFilter = () => {};
 
   return (
     <div>
       {/* 여기에 필터링 버튼들을 완성하세요. */}
-      <button onClick={filterByAge}>18세 이상</button>
-      <button onClick={filterByGrade}>A등급</button>
-      <button onClick={resetFilter}>필터 초기화</button>
+      <button>18세 이상</button>
+      <button>A등급</button>
+      <button>필터 초기화</button>
     </div>
   );
 }
