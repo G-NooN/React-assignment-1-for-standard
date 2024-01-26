d7136e3d7640998accd68642e44c5f9607172bd9
GNooNJo <jjjchoon@nate.com>
2024-01-26 23:57:33 +0900

    style: change to arrow function

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -6,13 +6,11 @@ function StudentList({ filteredStudents }) {
     <ul>
       {/* 여기에 학생 목록을 표시하는 로직을 작성하세요. */}
       {/* ex: 홍길동 (20세) - A등급 */}
-      {filteredStudents.map(function (student, index) {
-        return (
-          <li key={index}>
-            {student.name} ({student.age})세 - {student.grade}등급
-          </li>
-        );
-      })}
+      {filteredStudents.map((student, index) => (
+        <li key={index}>
+          {student.name} ({student.age})세 - {student.grade}등급
+        </li>
+      ))}
     </ul>
   );
 }
