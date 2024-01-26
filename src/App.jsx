7d8f354c89771a67ec676b0322f83ca47911c6fd
GNooNJo <jjjchoon@nate.com>
2024-01-26 22:07:35 +0900

    style: change to arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -17,19 +17,17 @@ function App() {
       <ul>
         {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
         {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
-        {filteredStudents.map(function (student, index) {
-          return (
-            <li key={index}>
-              <button
-                onClick={() => {
-                  alert(`${student.name} - 나이: ${student.age} / 점수: ${student.grade}`);
-                }}
-              >
-                {student.name}
-              </button>
-            </li>
-          );
-        })}
+        {filteredStudents.map((student, index) => (
+          <li key={index}>
+            <button
+              onClick={() => {
+                alert(`${student.name} - 나이: ${student.age} / 점수: ${student.grade}`);
+              }}
+            >
+              {student.name}
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
